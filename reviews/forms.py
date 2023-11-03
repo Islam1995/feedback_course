62b78b70cc7748c71648f3e68c847772c6cb7df2
islam1995 <islamellshaikh22@gmail.com>
2023-11-03 05:39:51 +0200

    used model form instead of form direct

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,17 +1,36 @@
 from django import forms 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label= "Your Name",
-                                max_length=20,
+from .models import Review
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label= "Your Name",
+#                                 max_length=20,
                       
-                                error_messages={
-                                    "required": "Your must entered your name!",
-                                    "max_length": "please entered short name!"
-                                }
-    )
-    review_text = forms.CharField(label="Your Feedback",
-                                  widget= forms.Textarea,
-                                  max_length=200
-                                  )
-    rating = forms.IntegerField(label="Your Rating", max_value=5, min_value=1)
-    
+#                                 error_messages={
+#                                     "required": "Your must entered your name!",
+#                                     "max_length": "please entered short name!"
+#                                 }
+#     )
+#     review_text = forms.CharField(label="Your Feedback",
+#                                   widget= forms.Textarea,
+#                                   max_length=200
+#                                   )
+#     rating = forms.IntegerField(label="Your Rating", max_value=5, min_value=1)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        #exculde = ['owner_comment']
+        fields = "__all__"
+        labels ={
+            "user_name":"Your Name",
+            "review_text": "Your Feedback",
+            "rating":"Your Rating"
+        } 
+        error_messages= {
+            "user_name":
+                {
+                    "required": "Your must entered your name!",
+                    "max_length": "please entered short name!"
+                }
+        }
+            
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-from .models import Review
+#from .models import Review
 # Create your views here.
 
 def index(request):
@@ -13,10 +13,11 @@ def index(request):
     #        return render(request, "reviews/reviews.html",{
     #            "has_error": True           })
         if form.is_valid():
-            review = Review(user_name= form.cleaned_data['user_name'],
-                            review_text= form.cleaned_data['review_text'],
-                            rating= form.cleaned_data['rating'])
-            review.save()
+            form.save()
+            # review = Review(user_name= form.cleaned_data['user_name'],
+            #                 review_text= form.cleaned_data['review_text'],
+            #                 rating= form.cleaned_data['rating'])
+            #review.save()
             #print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
     else:
